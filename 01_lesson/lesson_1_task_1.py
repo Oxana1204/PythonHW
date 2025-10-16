65ea5c8c77085553b5d88b2adba901c806bdef2b
Oxana1204 <son5106166@gmail.com>
2025-10-17 00:03:17 +0300

    01_lesson

--- 01_lesson/lesson_1_task_1.py
+++ 01_lesson/lesson_1_task_1.py
@@ -1,3 +1,2 @@
 my_name = 'Оксана'
-
 print(my_name)
---
+++ 01_lesson/lesson_1_task_5.py
@@ -0,0 +1,13 @@
+def num(num):
+    print(num, end='')
+    num(8)
+    num(8)
+    num(0)
+    num(0)
+    num(5)
+    num(5)
+    num(5)
+    num(3)
+    num(5)
+    num(3)
+    num(5)
